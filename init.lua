3f425155bb2e57fdd45a2c4fcbafaff4e3eebd8f
Kae Sluder <kaesluder@fastmail.com>
2024-10-06 14:16:47 -0400

    minor changes

--- init.lua
+++ init.lua
@@ -617,6 +617,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        markdown_oxide = {},
 
         lua_ls = {
           -- cmd = {...},
@@ -835,7 +836,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'default'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
